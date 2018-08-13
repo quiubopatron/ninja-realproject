5aa21b1f5bcc465be76223bc6b4ddb61c2697a9f
tomas.prat <tomas.prat@sm2baleares.es>
2018-08-13 14:08:16 +0200

    Insertando listados en plantillas

--- src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("example")
 public class ExampleController {
@@ -16,7 +19,7 @@ public class ExampleController {
     // Primeraforma
     @RequestMapping(value = "/exampleString", method = RequestMethod.GET)
     public String exampleString(Model model) {
-        model.addAttribute("person", new Person("Luis", 25));
+        model.addAttribute("personas", getPersonas());
         return EXAMPLE_VIEW;
 
     }
@@ -26,9 +29,25 @@ public class ExampleController {
     public ModelAndView exampleMAV(){
 
         ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
-        modelAndView.addObject("person", new Person("Peep", 50));
+        modelAndView.addObject("personas", getPersonas());
         return modelAndView;
 
 //        return new ModelAndView(EXAMPLE_VIEW);
-    };
+    }
+
+    private List<Person> getPersonas() {
+
+
+        List<Person> personas = new ArrayList<>();
+        Person person1 = new Person("Tomas", 37);
+        Person person2 = new Person("Vanessa", 31);
+        Person person3 = new Person("Alicia", 3);
+
+        personas.add(person1);
+        personas.add(person2);
+        personas.add(person3);
+
+        return personas;
+
+    }
 }
